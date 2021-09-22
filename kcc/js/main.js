2c78b874b0f7a701e4888e053d7ce1f5d91fddc1
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-22 19:50:59 +0900

    HEADER

--- kcc/js/main.js
+++ kcc/js/main.js
@@ -4,11 +4,13 @@ const mainMenuLists = header.querySelectorAll('.main-menu li');
 mainMenuLists.forEach(function(mainMenuList) {
   mainMenuList.addEventListener('mouseenter', function () {
     header.classList.add('active');
+    mainMenuList.classList.add('selected');
   });
 });
 mainMenuLists.forEach(function(mainMenuList) {
   mainMenuList.addEventListener('mouseleave', function () {
     header.classList.remove('active');
+    mainMenuList.classList.remove('selected');
   });
 });
 
