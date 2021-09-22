aba0f0983c523801d291eaea7ad68f77aad5c441
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-22 16:08:00 +0900

    JS

---
+++ kcc/js/main.js
@@ -0,0 +1,13 @@
+// const header = document.querySelector('header#header');
+// const ulEl = header.querySelector('.main-menu li ul');
+
+// ulEl.addEventListener('mouseenter', function (event) {
+//   ulEl.classList.remove('open');
+//   event.target.classList.add('open');
+//   header.classList.add('active');
+// });
+
+// header.addEventListener('mouseleave', function (event) {
+//   ulEl.classList.remove('open');
+//   header.classList.remove('active');
+// });
